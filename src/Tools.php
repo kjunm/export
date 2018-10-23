0dfea023e6ecd4cfc9ea0c754c219eec3a7af13e
王俊梅 <1404397383@qq.com>
2018-10-23 14:34:37 +0800

    test

---
+++ src/Tools.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wim
+ * Date: 2018/10/23
+ * Time: 14:28
+ */
+namespace wim\export;
+
+class Tools
+{
+    public function __construct()
+    {
+    }
+
+    public static function hello()
+    {
+        echo 'Hello,world!';
+    }
+}
---
+++ src/demo/demo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wim
+ * Date: 2018/10/23
+ * Time: 14:31
+ */
+namespace wim\export\demo;
+
+use wim\export\Tools;
+
+Tools::hello();
